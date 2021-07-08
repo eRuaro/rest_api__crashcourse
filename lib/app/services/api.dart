a916c863f23f54dac9f93521a387f6ec1e10ebbd
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-07-08 17:32:15 +0800

    edited api.dart

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,5 +1,13 @@
 import 'package:rest_api__crashcourse/app/services/api_keys.dart';
 
+enum EndPoint {
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
 class API {
   final String apiKey;
 
@@ -9,10 +17,23 @@ class API {
 
   static final String host = 'ncov2019-admin.firebaseapp.com';
 
-  
   Uri tokenUri() => Uri(
-      scheme: 'https',
-      host: host,
-      path: 'token',
+    scheme: 'https',
+    host: host,
+    path: 'token',
   );
+
+  Uri endpointUri(EndPoint endpoint) => Uri(
+    scheme: 'https',
+    host: host,
+    path: _paths[endpoint],
+  );
+
+  static Map<EndPoint, String> _paths = {
+    EndPoint.cases: 'cases',
+    EndPoint.casesConfirmed: 'casesConfirmed',
+    EndPoint.casesSuspected: 'casesSuspected',
+    EndPoint.deaths: 'deaths',
+    EndPoint.recovered: 'recovered',
+  };
 }
