ad465726c8f6cd6423dd3362fe267e84a5a11696
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-07-08 17:41:11 +0800

    edited api service

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:rest_api__crashcourse/app/services/api.dart';
@@ -23,7 +24,43 @@ class APIService {
         return accessToken;
       }
     }
-    print('Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    print(
+        'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
     throw response;
   }
+
+  Future<int> getEndpointData({
+    required String accessToken,
+    required EndPoint endpoint,
+  }) async {
+    final uri = api.endpointUri(endpoint);
+    final response = await http.get(
+      Uri.parse(uri.toString()),
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint]!;
+        final int result = endpointData[responseJsonKey];
+        return result;
+      }
+    }
+
+    print(
+        'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
+  }
+
+  static Map<EndPoint, String> _responseJsonKeys = {
+    EndPoint.cases: 'cases',
+    EndPoint.casesConfirmed: 'cases_confirmed',
+    EndPoint.casesSuspected: 'cases_suspected',
+    EndPoint.deaths: 'deaths',
+    EndPoint.recovered: 'recovered',
+  };
 }
