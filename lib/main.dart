82e7e5fb7810ca1da4b4a682fd93fe21d59bd9d9
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-07-08 11:23:44 +0800

    added things

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'HomePage',
+      ),
     );
   }
 }
