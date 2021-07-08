0958ff6cb01894ac1420ff22dc610fa7f8901fcf
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-07-08 16:51:02 +0800

    created new class

---
+++ lib/app/services/api.dart
@@ -0,0 +1,17 @@
+import 'package:rest_api__crashcourse/app/services/api_keys.dart';
+
+class API {
+  final String apiKey;
+
+  const API({required this.apiKey});
+
+  factory API.sandbox() => API(apiKey: APIKeys.sandboxKey);
+
+  static final String host = 'ncov2019-admin.firebaseapp.com';
+
+  Uri tokenUri() => Uri(
+      scheme: 'https',
+      host: host,
+      path: 'token',
+  );
+}
